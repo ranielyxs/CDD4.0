52e26c07cdafb670379efd0016ed1cc70375c286
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-05-04 10:04:21 -0300

    Add files via upload

---
+++ questao9.py
@@ -0,0 +1,9 @@
+idade=int(input("Idade:"))
+meses=int(input("Meses:"))
+dias=int(input("Dias:"))
+
+soma= (365*idade + 30*meses + dias)
+print("A quantidade de dias vividos até agora é:",soma,"dias")
+
+
+
