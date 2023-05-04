a1dfff9cc8b8eefceab42bdf4a218e52e2977a08
Raniely Paulino <128312595+ranielyxs@users.noreply.github.com>
2023-05-04 10:25:00 -0300

    Delete questao9.py

--- questao9.py
+++
@@ -1,9 +0,0 @@
-idade=int(input("Idade:"))
-meses=int(input("Meses:"))
-dias=int(input("Dias:"))
-
-soma= (365*idade + 30*meses + dias)
-print("A quantidade de dias vividos até agora é:",soma,"dias")
-
-
-
